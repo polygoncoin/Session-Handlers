112212fd5d473b9eb8686e2bbf367f4d49739a70
“polygoncoin <polygon.co.in@gmail.com>
2024-10-15 19:36:13 +0530

    Refactoring

--- Login.php
+++ Login.php
@@ -1,15 +1,15 @@
 <?php
 include __DIR__ . '/SessionHandlers/Session.php';
 
+// Initialise Session Handler
+Session::initSessionHandler('File');
+
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     if (verifyCredentials($username, $password)) {
         $userDetails = getFromDB($username);
         
-        // Initialise Session Handler
-        Session::initSessionHandler('File');
-
         // Start session in normal (read/write) mode.
         Session::start_rw_mode();
         $_SESSION = $userDetails;
