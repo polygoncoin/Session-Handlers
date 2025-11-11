c3f2735a21adf9bc068147769523203d08750ee5
“polygoncoin <polygon.co.in@gmail.com>
2025-11-11 19:56:47 +0530

    Refactoring

--- CustomSessionHandler/Session.php
+++ CustomSessionHandler/Session.php
@@ -33,6 +33,13 @@ use CustomSessionHandler\Containers\SessionContainerInterface;
 class Session
 {
     /**
+     * Domain Name
+     *
+     * @var null|string
+     */
+    public static $sessionDomain = 'localhost';
+
+    /**
      * SET THESE TO ENABLE ENCRYPTION
      * ENCRYPTION PASS PHRASE
      *
@@ -368,12 +375,14 @@ class Session
             'cookie_lifetime' => 0,
             'cookie_path' => '/',
             'cookie_domain' => '',
-            'cookie_secure' => ((strpos(
-                haystack: $_SERVER['HTTP_HOST'],
-                needle: 'localhost'
-            ) === false) ? true : false),
+            'cookie_secure' => (
+                !in_array(
+                    'localhost',
+                    explode('.', self::$sessionDomain)
+                )
+            ) ? true : false,
             'cookie_httponly' => true,
-            'cookie_samesite' => 'Strict'
+            'cookie_samesite' => 'Lax'
         ];
 
         if (self::$sessionMode === 'File') {
@@ -438,6 +447,7 @@ class Session
         ) {
             $options = self::$options;
             $options['read_and_close'] = true;
+            self::$sessionContainer->sessionOptions = $options;
 
             return session_start(options: $options);
         }
@@ -451,6 +461,7 @@ class Session
      */
     public static function sessionStartReadWrite(): bool
     {
+        self::$sessionContainer->sessionOptions = self::$options;
         return session_start(options: self::$options);
     }
 
